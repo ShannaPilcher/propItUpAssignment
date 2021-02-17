63babc94161960f752f77f5d04bd33028abab0a6
Shanna Pilcher <shannampilcher@gmail.com>
2021-02-17 14:28:24 -0700

    Added PersonComponent with props.

---
+++ src/components/PersonComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+
+    class PersonCard extends Component {
+        render (){
+            const {firstName, lastName, age, hairColor} = this.props;
+            return(
+                <div>
+                    <h1>{lastName}, {firstName}</h1>
+                    <p>Age: {age}</p>
+                    <p>Hair Color: {hairColor}</p>
+                </div>
+            )
+        }
+    }
